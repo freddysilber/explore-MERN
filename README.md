25c858854af191918ccf7be63d943cf3c8a58683
freddysilber <freddy.silber@gmail.com>
2020-09-20 14:41:12 -0600

    readme

--- README.md
+++ README.md
@@ -4,3 +4,8 @@ Sample application for learning the MonogDb, Express, React, and Node.js stack w
 
 # Getting Started
 Start the server on port 5000 and the client on port 3000 ```npm run dev```
+
+# References
+
+MongoDb note:
+[Blog for help setting up the Db with mongo](https://blog.bitsrc.io/build-a-login-auth-app-with-mern-stack-part-1-c405048e3669)
--- client/src/actions/authActions.js
+++ client/src/actions/authActions.js
@@ -4,7 +4,6 @@ import jwt_decode from 'jwt-decode'
 import { GET_ERRORS, SET_CURRENT_USER, USER_LOADING } from './types'
 // Register User
 export const registerUser = (userData, history) => dispatch => {
-	console.log('userData', userData)
 	axios.post('/api/users/register', userData)
 		.then(res => history.push('/login'))
 		.catch(err =>
@@ -18,7 +17,6 @@ export const registerUser = (userData, history) => dispatch => {
 export const loginUser = userData => dispatch => {
 	axios.post('/api/users/login', userData)
 		.then(res => {
-			console.log('RES from login user action', res)
 			// Save to localStorage
 			// Set token to localStorage
 			const { token } = res.data
