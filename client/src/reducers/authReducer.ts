0a15e7319aafb6d8ce7705ce21fd0a51188a7d33
freddysilber <freddy.silber@gmail.com>
2020-09-20 16:44:50 -0600

    ts refactor

--- client/src/reducers/authReducer.ts
+++ client/src/reducers/authReducer.ts
@@ -2,7 +2,7 @@ import { SET_CURRENT_USER, USER_LOADING } from '../actions/types'
 
 const isEmpty = require('is-empty')
 
-type InitialState = {
+interface InitialState {
 	isAuthenticated: boolean,
 	user: object,
 	loading: boolean
--- client/src/reducers/errorReducer.ts
+++ client/src/reducers/errorReducer.ts
@@ -1,6 +1,6 @@
 import { GET_ERRORS } from '../actions/types'
 
-type InitialState = {}
+interface InitialState { }
 
 const initialState: InitialState = {}
 
